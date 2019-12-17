27bcbe9952809437c1e219412b74bbba42afe9a9
Plane-walker <a990990@163.com>
2019-12-17 20:24:41 +0800

    Tab to block. #115

--- test/func.c
+++ test/func.c
@@ -1,12 +1,12 @@
 int wrongplus(int a)
 {
-	if(a == 1)
-		return 1;
+    if(a == 1)
+        return 1;
     return wrongplus(a - 1) * a;
 }
 int main(){
     int a = 4;
-	input(&a);
+    input(&a);
     print(wrongplus(a));
     return 0;
 }
